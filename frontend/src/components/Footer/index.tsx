996619375963b87b889dcf7e7a8b2e075337b730
Felipe Veiga <felipeveigadossantos@gmail.com>
2021-11-02 21:01:55 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/felipevsantos" target="_blank" rel="noreferrer">Felipe Veiga Dos Santos</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Contato: <a href="https://www.linkedin.com/in/felipe-veiga-dos-santos-35b451183/" target="_blank" rel="noreferrer">@felipeveigadossantos</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+
+export default Footer;
